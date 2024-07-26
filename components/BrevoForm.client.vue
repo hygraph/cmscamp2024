0c02ce5f4c751827eaf93906970dc83201af7bf0
Tim Benniks <tbenniks@gmail.com>
2024-07-26 12:44:46 +0200

    updated brevo form

--- components/BrevoForm.client.vue
+++ components/BrevoForm.client.vue
@@ -29,7 +29,7 @@ useHead({
 </script>
 
 <template>
-  <div class="sib-form" style="text-align: center; background-color: #000">
+  <div class="sib-form" style="text-align: center; background-color: #eff2f7">
     <div id="sib-form-container" class="sib-form-container">
       <div
         id="error-message"
@@ -91,7 +91,7 @@ useHead({
       <div></div>
       <div
         id="sib-container"
-        class="sib-container--large sib-container--vertical"
+        class="sib-container--large sib-container--horizontal"
         style="
           text-align: center;
           background-color: rgba(255, 255, 255, 1);
@@ -106,14 +106,14 @@ useHead({
         <form
           id="sib-form"
           method="POST"
-          action="https://9f6eaad7.sibforms.com/serve/MUIFADJCPaKa2weiAejsfzTMt4z2z45YsOIAh8IpMW_FpQhlw6aDZGj8e4hXh0Uxj6aYSWPu3m7GnunbEqVDfNEtxR_n_LEXJmtGexpldCBOobxjLWsTLcWuAtFw4sPPA4xp4PMljT14nWgHK-_n-HcO0OhvIxolZYcv_hrCBHy8Aomb05xTkiUruHW8bS27CW2naalFHoVic3CU"
+          action="https://9f6eaad7.sibforms.com/serve/MUIFAHjt8c-EyVFhpCOcOzcBhQrawgeEPqn9rE21YiHBP6O3h39qCpooH0uHmHNH__ALbrVRGjs84cAHm-sczUh26XafCUDXmPjNCruxqIK8Z9tPfVUil6TUUi0PRTspzvX81Je61gvltk5ge1OO8ZdzXLeSElP-DlzGm9mwRSWjIRmALDYoNKmFGzj5PdGelEJymWD630HU6bem"
           data-type="subscription"
         >
           <div style="padding: 8px 0">
             <div
               class="sib-form-block"
               style="
-                font-size: 26px;
+                font-size: 24px;
                 text-align: left;
                 font-weight: 700;
                 font-family: 'Helvetica', sans-serif;
@@ -138,30 +138,14 @@ useHead({
               "
             >
               <div class="sib-text-form-block">
-                <p>Subscribe to our newsletter to stay up to date.</p>
+                <p>Subscribe to our Newsletter.</p>
               </div>
             </div>
           </div>
           <div style="padding: 8px 0">
             <div class="sib-input sib-form-block">
               <div class="form__entry entry_block">
-                <div class="form__label-row">
-                  <label
-                    class="entry__label"
-                    style="
-                      font-weight: 700;
-                      text-align: left;
-                      font-size: 16px;
-                      text-align: left;
-                      font-weight: 700;
-                      font-family: 'Helvetica', sans-serif;
-                      color: #3c4858;
-                    "
-                    for="EMAIL"
-                    data-required="*"
-                    >Enter your e-mail address to sign up.</label
-                  >
-
+                <div class="form__label-row form__label-row--horizontal">
                   <div class="entry__field">
                     <input
                       class="input"
@@ -189,26 +173,13 @@ useHead({
                   "
                 >
                 </label>
-                <label
-                  class="entry__specification"
-                  style="
-                    font-size: 12px;
-                    text-align: left;
-                    font-family: 'Helvetica', sans-serif;
-                    color: #8390a4;
-                    text-align: left;
-                  "
-                >
-                  Please enter your e-mail address for registration, e.g.
-                  abc@xyz.com.
-                </label>
               </div>
             </div>
           </div>
           <div style="padding: 8px 0">
-            <div class="sib-optin sib-form-block">
+            <div class="sib-optin sib-form-block" data-required="true">
               <div class="form__entry entry_mcq">
-                <div class="form__label-row">
+                <div class="form__label-row form__label-row--horizontal">
                   <div class="entry__choice" style="">
                     <label>
                       <input
@@ -217,6 +188,7 @@ useHead({
                         value="1"
                         id="OPT_IN"
                         name="OPT_IN"
+                        required
                       />
                       <span
                         class="checkbox checkbox_tick_positive"
@@ -231,14 +203,15 @@ useHead({
                           background-color: transparent;
                         "
                         ><p>
-                          I would like to receive your newsletter and accept the
-                          <a
-                            href="https://www.brevo.com/en/legal/privacypolicy/"
-                          >
-                            Brevo's Privacy Policy.</a
-                          >
+                          Yes, I'd like to receive your newsletter and accept
+                          the privacy policy.
                         </p>
-                      </span>
+                        <span
+                          data-required="*"
+                          style="display: inline"
+                          class="entry__label entry__label_optin"
+                        ></span
+                      ></span>
                     </label>
                   </div>
                 </div>
@@ -255,23 +228,53 @@ useHead({
                   "
                 >
                 </label>
-                <label
-                  class="entry__specification"
-                  style="
-                    font-size: 12px;
-                    text-align: left;
-                    font-family: 'Helvetica', sans-serif;
-                    color: #8390a4;
-                    text-align: left;
-                  "
-                >
-                  You can unsubscribe from the newsletter at any time via the
-                  link in our newsletter.
-                </label>
               </div>
             </div>
           </div>
-
+          <div style="padding: 8px 0">
+            <div class="sib-form__declaration" style="direction: ltr">
+              <div class="declaration-block-icon">
+                <svg
+                  class="icon__SVG"
+                  width="0"
+                  height="0"
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <defs>
+                    <symbol id="svgIcon-sphere" viewBox="0 0 63 63">
+                      <path
+                        class="path1"
+                        d="M31.54 0l1.05 3.06 3.385-.01-2.735 1.897 1.05 3.042-2.748-1.886-2.738 1.886 1.044-3.05-2.745-1.897h3.393zm13.97 3.019L46.555 6.4l3.384.01-2.743 2.101 1.048 3.387-2.752-2.1-2.752 2.1 1.054-3.382-2.745-2.105h3.385zm9.998 10.056l1.039 3.382h3.38l-2.751 2.1 1.05 3.382-2.744-2.091-2.743 2.091 1.054-3.381-2.754-2.1h3.385zM58.58 27.1l1.04 3.372h3.379l-2.752 2.096 1.05 3.387-2.744-2.091-2.75 2.092 1.054-3.387-2.747-2.097h3.376zm-3.076 14.02l1.044 3.364h3.385l-2.743 2.09 1.05 3.392-2.744-2.097-2.743 2.097 1.052-3.377-2.752-2.117 3.385-.01zm-9.985 9.91l1.045 3.364h3.393l-2.752 2.09 1.05 3.393-2.745-2.097-2.743 2.097 1.05-3.383-2.751-2.1 3.384-.01zM31.45 55.01l1.044 3.043 3.393-.008-2.752 1.9L34.19 63l-2.744-1.895-2.748 1.891 1.054-3.05-2.743-1.9h3.384zm-13.934-3.98l1.036 3.364h3.402l-2.752 2.09 1.053 3.393-2.747-2.097-2.752 2.097 1.053-3.382-2.743-2.1 3.384-.01zm-9.981-9.91l1.045 3.364h3.398l-2.748 2.09 1.05 3.392-2.753-2.1-2.752 2.096 1.053-3.382-2.743-2.102 3.384-.009zM4.466 27.1l1.038 3.372H8.88l-2.752 2.097 1.053 3.387-2.743-2.09-2.748 2.09 1.053-3.387L0 30.472h3.385zm3.069-14.025l1.045 3.382h3.395L9.23 18.56l1.05 3.381-2.752-2.09-2.752 2.09 1.053-3.381-2.744-2.1h3.384zm9.99-10.056L18.57 6.4l3.393.01-2.743 2.1 1.05 3.373-2.754-2.092-2.751 2.092 1.053-3.382-2.744-2.1h3.384zm24.938 19.394l-10-4.22a2.48 2.48 0 00-1.921 0l-10 4.22A2.529 2.529 0 0019 24.75c0 10.47 5.964 17.705 11.537 20.057a2.48 2.48 0 001.921 0C36.921 42.924 44 36.421 44 24.75a2.532 2.532 0 00-1.537-2.336zm-2.46 6.023l-9.583 9.705a.83.83 0 01-1.177 0l-5.416-5.485a.855.855 0 010-1.192l1.177-1.192a.83.83 0 011.177 0l3.65 3.697 7.819-7.916a.83.83 0 011.177 0l1.177 1.191a.843.843 0 010 1.192z"
+                        fill="#0092FF"
+                      ></path>
+                    </symbol>
+                  </defs>
+                </svg>
+                <svg class="svgIcon-sphere" style="width: 54px; height: 54px">
+                  <use xlink:href="#svgIcon-sphere"></use>
+                </svg>
+              </div>
+              <div
+                style="
+                  font-size: 12px;
+                  text-align: left;
+                  font-family: 'Helvetica', sans-serif;
+                  color: #687484;
+                  background-color: transparent;
+                "
+              >
+                <p>
+                  By submitting this form you agree that the personal data you
+                  provided will be transferred to our marketing platform Brevo
+                  for processing in accordance with
+                  <a href="https://www.brevo.com/en/legal/privacypolicy/"
+                    >Brevo's Privacy Policy.</a
+                  >
+                </p>
+              </div>
+            </div>
+          </div>
           <div style="padding: 8px 0">
             <div class="sib-form-block" style="text-align: left">
               <button
